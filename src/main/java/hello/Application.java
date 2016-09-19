701f10b1c907a15bff8178e466d2ab590d742927
喵叔catuncle <andych008@163.com>
2016-09-19 16:23:11 +0800

    关闭banner

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -4,17 +4,22 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import hello.controller.FileUploadController;
+import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.FileSystemUtils;
 
 @SpringBootApplication
+@ServletComponentScan
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+//        SpringApplication.run(Application.class, args);
+
+        new SpringApplicationBuilder().bannerMode(Banner.Mode.OFF).sources(Application.class).run(args);
     }
 
     @Bean
