2ada21e30db792402f1da4062f54d0724f805a7c
喵叔catuncle <andych008@163.com>
2016-09-20 18:38:11 +0800

    使用`@PropertySource`注解加载`*.properties`中的配置

--- src/main/java/hello/api/MailController.java
+++ src/main/java/hello/api/MailController.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonView;
 import hello.config.MailConfiguration;
 import hello.model.ResponseResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +15,19 @@ public class MailController {
     @Autowired
     private MailConfiguration mailConfiguration;
 
+    @Value("${mail.host}")
+    private String host;
 
     @JsonView(MailConfiguration.Show.class)
     @RequestMapping()
     public ResponseResult<MailConfiguration> findAll() {
         return ResponseResult.<MailConfiguration>newBuilder()
                 .build(mailConfiguration);
+    }
+
+    @RequestMapping("host")
+    public ResponseResult<String> host() {
+        return ResponseResult.<String>newBuilder()
+                .build(host);
     }
 }
